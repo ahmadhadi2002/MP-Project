729677d3ae74f75b6a455eaa946648a517ff217a
ahmadhadi2002 <ahmadhadi200228@gmail.com>
2022-12-27 16:28:40 +0800

    small

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <html>
-    <body>
-        <iframe src="ui/navbar.html" frameborder ="0" style="width:100%;height:100%;"></iframe>
+    <body style="margin:0;">
+        <iframe src="ui/navbar.html" frameborder ="0" style="width:100%;height:;"></iframe>
         <iframe src="ui/slideshow.html" frameborder="0" style="width:100%;height:100%;"></iframe>
         <iframe src="ui/sidenav.html" frameborder="0" style="width: 100%;height: 100%;"></iframe>
     <body>
