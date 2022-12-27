27dc31bf60f5848b3368ea4584e8669def27bf0d
SexyBeast21 <kardellkb2204@gmail.com>
2022-12-27 16:55:31 +0800

    as

--- main.php
+++ main.php
@@ -1,7 +1,16 @@
 <html>
+<head>
+<style>
+
+    body {
+        margin=0;
+    }
+</style>
+</head>
+
     <body style="margin:0;">
         <iframe src="ui/navbar.html" frameborder ="0" style="width:100%;height:;"></iframe>
         <iframe src="ui/slideshow.html" frameborder="0" style="width:100%;height:100%;"></iframe>
-        <iframe src="ui/sidenav.html" frameborder="0" style="width: 100%;height: 100%;"></iframe>
+       <!-- <iframe src="ui/sidenav.html" frameborder="0" style="width: 100%;height: 100%;"></iframe> -->
     <body>
 </html>
